709c2ee2d4e49344fe9cdbf870b0c56e24dc9850
Samuel <samif3443@gmail.com>
2023-09-06 06:12:04 -0700

    Add to schema_based_on_diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,4 @@ psql -U postgres clinic-database
 CREATE TABLE patients (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,name VARCHAR(50),date_of_birth DATE);
 CREATE TABLE medical_history (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,admitted_at TIMESTAMP,patient_id INT REFERENCES patients(id),STATUS VARCHAR(50));
 CREATE TABLE invoices (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,total_amount DECIMAL,generated_at TIMESTAMP,payed_at TIMESTAMP, medical_history_id INT REFERENCES medical_history(id));
+CREATE TABLE invoice_items(id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, unit_price DECIMAL, quantity INT, total_price DECIMAL, invoice_id INT REFERENCES invoices(id), treatment_id INT);
